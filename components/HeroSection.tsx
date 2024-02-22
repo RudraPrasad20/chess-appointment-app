240ec639cb5eaf6da4ad3a1e1714d0280cd58c52
RUDRA <rudra@RUDRAs-MacBook-Air.local>
2024-02-23 00:48:12 +0530

    implimented courses

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,10 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { Button } from './ui/moving-border'
 
 const HeroSection = () => {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
         <div className="p-4 relative z-10 w-full text-center">
+        <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
         <h1
             className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
             >Master the art of music</h1>
@@ -13,7 +19,12 @@ Dive into our comprehensive music courses and transform your musical journey tod
 </p>
         <div className="mt-4">
             <Link href={"/courses"}>
-                Explore Courses
+            <Button
+        borderRadius="1.75rem"
+        className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+      >
+        Explore Courses
+      </Button>
             </Link>
         </div>
         </div>
