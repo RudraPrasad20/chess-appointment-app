d72984d3f4916574947f8710925fa2ab63df84b7
RUDRA <rudra@RUDRAs-MacBook-Air.local>
2024-02-23 23:35:41 +0530

    updated courses

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -45,7 +45,7 @@ const Courses = () => {
                   <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
                     {course.description}
                   </p>
-                  <Link href={`/courses${course.slug}`}>Learn More</Link>
+                  <Link href={`/courses${course.slug}`} >Learn More</Link>
                 </div>
               </BackgroundGradient>
             </div>
@@ -53,9 +53,14 @@ const Courses = () => {
         </div>
       </div>
 
-      <div>
-        <Link href={"/courses"}>Learn More</Link>
-      </div>
+      <div className="mt-20 text-center">
+            <Link href={"/courses"}
+            className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
+            >
+            View All courses
+            </Link>
+        </div>
+
     </div>
   );
 };
